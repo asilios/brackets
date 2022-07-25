d2cab90c9cc892b550a381de6ac75b73a50e9018
asilios <85323192+asilios@users.noreply.github.com>
2022-07-25 23:38:48 +0500

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const confDict = {};
+    const stackBr = [];
+    bracketsConfig.forEach(([open, close]) => confDict[open] = close);
+    for (let i = 0; i < str.length; i++) {
+        if (stackBr.length) {
+            if (confDict[stackBr[stackBr.length - 1]] === str[i]) {
+                stackBr.pop();
+                continue;
+            }
+        }
+        if (confDict[str[i]]){
+            stackBr.push(str[i]);
+            continue;
+        }
+        return false;
+    }
+    return !stackBr.length;
 }
